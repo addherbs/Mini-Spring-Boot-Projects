01c2c2412933def6242d83f20e0329ec978ecc68
Aditya Pandey <addherbs@gmail.com>
2021-04-29 00:28:21 -0500

    Using RepositoryRestResource annotation to enable all the rest features. No mappings required!

--- SpringDataRest/src/main/java/com/example/springdatarest/PersonRepo.java
+++ SpringDataRest/src/main/java/com/example/springdatarest/PersonRepo.java
@@ -2,7 +2,10 @@ package com.example.springdatarest;
 
 import com.example.springdatarest.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(collectionResourceRel = "persons", path = "persons")
 public interface PersonRepo extends JpaRepository<Person, Integer> {
 
 }
