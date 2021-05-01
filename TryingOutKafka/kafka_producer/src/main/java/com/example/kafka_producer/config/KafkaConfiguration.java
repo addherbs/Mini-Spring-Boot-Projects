343d3c0117a58e7cdd476c239699251c450a6a80
Aditya Pandey <addherbs@gmail.com>
2021-04-30 22:48:25 -0500

    kafka producer configuration

---
+++ TryingOutKafka/kafka_producer/src/main/java/com/example/kafka_producer/config/KafkaConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.kafka_producer.config;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        System.out.println("Came to ProducerFactory<String, String> producerFactory");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        System.out.println("Came to KafkaTemplate<String, String> kafkaTemplate");
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
